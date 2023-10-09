1dc2b715cf70a72ed66bfc132f3152e959e095b7
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2023-10-09 16:28:04 +0200

    Twig end

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/book')]
 class BookController extends AbstractController
 {
-    #[Route('', name: 'app_book')]
+    #[Route('', name: 'app_book_index')]
     public function index(): Response
     {
         return $this->render('book/index.html.twig', [
