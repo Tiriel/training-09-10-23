34ab28b8fb93797b11ae61e38834327426892d00
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2023-10-10 14:49:56 +0200

    Contact DTO

---
+++ src/Contact/Dto/Contact.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Contact\Dto;
+
+class Contact
+{
+    public function __construct(
+        private string $name,
+        private string $email,
+        private string $subject,
+        private string $message,
+        private ?\DateTimeImmutable $createdAt = null,
+    ) {}
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): Contact
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): Contact
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    public function getSubject(): string
+    {
+        return $this->subject;
+    }
+
+    public function setSubject(string $subject): Contact
+    {
+        $this->subject = $subject;
+        return $this;
+    }
+
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): Contact
+    {
+        $this->message = $message;
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(?\DateTimeImmutable $createdAt): Contact
+    {
+        $this->createdAt = $createdAt;
+        return $this;
+    }
+}
