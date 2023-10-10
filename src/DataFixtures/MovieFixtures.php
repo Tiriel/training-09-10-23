8e5c989d396374ecc11d26886d2ec6304f422790
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2023-10-10 14:40:05 +0200

    Doctrine end

--- src/DataFixtures/MovieFixtures.php
+++ src/DataFixtures/MovieFixtures.php
@@ -36,13 +36,13 @@ class MovieFixtures extends Fixture
                 //->setImdbId($datum['imdbID'])
             ;
 
-            //foreach (explode(', ', $datum['Genre']) as $genreName) {
-            //    $genre = \array_key_exists($genreName, $genres)
-            //        ? $genres[$genreName]
-            //        : $genres[$genreName] = (new Genre())->setName($genreName);
-            //
-            //    $movie->addGenre($genre);
-            //}
+            foreach (explode(', ', $datum['Genre']) as $genreName) {
+                $genre = \array_key_exists($genreName, $genres)
+                    ? $genres[$genreName]
+                    : $genres[$genreName] = (new Genre())->setName($genreName);
+
+                $movie->addGenre($genre);
+            }
 
             yield $movie;
         }
