aec34a3c1be783c2f7cfd5bd10b372d75992ea3c
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2023-10-12 16:36:05 +0200

    End of events

---
+++ src/EventSubscriber/UserSubscriber.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
+use Symfony\Component\Security\Http\SecurityEvents;
+
+class UserSubscriber implements EventSubscriberInterface
+{
+    public function __construct(private readonly EntityManagerInterface $manager)
+    {
+    }
+
+    public function onSecurityInteractiveLogin(InteractiveLoginEvent $event): void
+    {
+        $user = $event->getAuthenticationToken()->getUser();
+
+        if ($user instanceof User) {
+            $user->setLastConnectedAt(new \DateTimeImmutable());
+            $this->manager->persist($user);
+            $this->manager->flush();
+        }
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            SecurityEvents::INTERACTIVE_LOGIN => 'onSecurityInteractiveLogin',
+        ];
+    }
+}
